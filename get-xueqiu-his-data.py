de47c289768c726827ca39cfcbdf23f2847769fb
Phoenix Zhang <pho.coderzhang@gmail.com>
2016-10-16 20:23:29 +0800

    fix

--- get-xueqiu-his-data.py
+++ get-xueqiu-his-data.py
@@ -2,6 +2,7 @@
 from urllib import request
 import pandas as pd
 import io
+import numpy as np
 
 hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '
        + 'AppleWebKit/537.11 (KHTML, like Gecko) '
@@ -28,7 +29,7 @@ def get_data_by_date(df, dt):
 
 
 def calc_row_data(tag, df):
-    df[tag + '-diff-price'] = df['open'] - df['close']
+    df[tag + '-diff-price'] = df['close'] - df['open']
     df[tag + '-diff-price-rate'] = df[tag + '-diff-price'] / df['open']
     df[tag + '-max-diff-price'] = df['high'] - df['low']
     df.rename(columns={'open': tag + '-open',
@@ -40,32 +41,50 @@ def calc_row_data(tag, df):
     return df
 
 
-def up_up_down_down(diff_price1, diff_price2):
-    if diff_price1 > 0 and diff_price2 > 0:
+def up_up_down_down(row, tag1, tag2):
+    if row[tag1 + '-diff-price'] > 0 and row[tag2 + '-diff-price'] > 0:
         return True
-    elif diff_price1 < 0 and diff_price2 < 0:
+    elif row[tag1 + '-diff-price'] < 0 and row[tag2 + '-diff-price'] < 0:
         return True
+    elif row[tag1 + '-diff-price'] == 0 or row[tag2 + '-diff-price'] == 0:
+        return None
     else:
         return False
 
 
-def judge_rules(tag1, tag2, df):
-    df['right'] = up_up_down_down(df[tag1 + '-diff-price'],
-                                  df[tag2 + '-diff-price'])
-    return df
+def calc_income(row, tag):
+    if row['right'] is True:
+        return np.abs(row[tag + '-diff-price'])
+    elif row['right'] is False:
+        return np.negative(np.abs(row[tag + '-diff-price']))
+    elif row['right'] is None:
+        return 0
 
 
-def test():
-    hs300 = get_history_data('SH000300')
-    chau = get_history_data('CHAU')
-    calc_row_data('SH000300', hs300)
-    calc_row_data('CHAU', chau)
+def get_re_data(tag1, tag2, dt1, dt2=None):
+    hs300 = get_history_data(tag1)
+    chau = get_history_data(tag2)
+    calc_row_data(tag1, hs300)
+    calc_row_data(tag2, chau)
     merge_data = pd.merge(hs300, chau, on='date')
-    merge_data['right'].apply()
-    for i, row in merge_data.iterrows():
-        if row['date'] >= pd.tslib.Timestamp("2016-10-01"):
-            print(row)
+    merge_data['right'] = merge_data.apply(lambda row:
+                                           up_up_down_down(row, tag1, tag2),
+                                           axis=1)
+    merge_data['income'] = merge_data.apply(lambda row:
+                                            calc_income(row, tag2),
+                                            axis=1)
+    if dt2 is not None:
+        return merge_data[(merge_data['date'] >= pd.tslib.Timestamp(dt1)) &
+                          (merge_data['date'] < pd.tslib.Timestamp(dt2))]
+    else:
+        return merge_data[merge_data['date'] >= pd.tslib.Timestamp(dt1)]
+
+
+def calc_re_data(tag, df):
+    return None
 
 
 if __name__ == '__main__':
-    get_history_data("SH000300")
+    tag1 = 'SH000300'
+    tag2 = 'CHAU'
+    re = get_re_data(tag1, tag2, '2016-08-01')
